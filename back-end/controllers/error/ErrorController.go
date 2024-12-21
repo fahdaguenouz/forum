d2cb3cb27175764245c74d3a4ab964b332fe83df
Fahd Aguenouz <you@example.com>
2024-12-21 16:46:24 +0100

    chnaging the error controller to adjust the root

--- back-end/controllers/error/ErrorController.go
+++ back-end/controllers/error/ErrorController.go
@@ -7,27 +7,28 @@ import (
 )
 
 func ErrorController(w http.ResponseWriter, r *http.Request, statusCode int) {
-    // Set the status code manually before rendering the error page
     w.WriteHeader(statusCode)
 
+    // Build the absolute path to the template
+    basePath, _ := os.Getwd() // Get the current working directory
+    templatePath := filepath.Join(basePath, "Front-end/views/error/error.html")
+
     // Parse the error template
-    tmp, err := template.ParseFiles("Front-end/views/error/error.html")
+    tmp, err := template.ParseFiles(templatePath)
     if err != nil {
-        // If parsing fails, send an internal server error without calling WriteHeader again
         http.Error(w, err.Error()+"fahd", http.StatusInternalServerError)
         return
     }
 
-    // Define the error type to pass to the template
     errorType := models.Error{
         StatusCode:   statusCode,
         ErrorMessage: http.StatusText(statusCode),
     }
 
-    // Render the template with the error message
     if err := tmp.Execute(w, errorType); err != nil {
-        // If rendering fails, send an internal server error without calling WriteHeader again
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
+    }
+}       return
     }
 }
