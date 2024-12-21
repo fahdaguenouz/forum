1c3ee87ab8966b408717e5731b76f511ef472835
Fahd Aguenouz <you@example.com>
2024-12-21 15:49:10 +0100

    update the home

--- back-end/controllers/error/ErrorController.go
+++ back-end/controllers/error/ErrorController.go
@@ -7,20 +7,27 @@ import (
 )
 
 func ErrorController(w http.ResponseWriter, r *http.Request, statusCode int) {
-	tmp, err := template.ParseFiles("Front-end/views/error/error.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	errorType := models.Error{
-		StatusCode:   statusCode,
-		ErrorMessage: http.StatusText(statusCode),
-	}
-	w.WriteHeader(statusCode)
-	// fmt.Println("rrrrrrr")
-	if err := tmp.Execute(w, errorType); err != nil {
-		// fmt.Println("error:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    tmp, err := template.ParseFiles("Front-end/views/error/error.html")
+    if err != nil {
+        // Set the status code manually for the error response
+        w.WriteHeader(http.StatusInternalServerError)
+        // Use http.Error to send the error message in the body
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Set the status code manually before rendering the error page
+    w.WriteHeader(statusCode)
+
+    errorType := models.Error{
+        StatusCode:   statusCode,
+        ErrorMessage: http.StatusText(statusCode),
+    }
+
+    // Render the template with the error message
+    if err := tmp.Execute(w, errorType); err != nil {
+        // If rendering fails, send an internal server error
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module Forum
 
-
-go 1.20
-
+go 1.22.3
 
 require github.com/mattn/go-sqlite3 v1.14.24 // indirect
